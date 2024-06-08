dd2ea30bd46e2ce651d01f0f13fe89aa7bdb584f
Johany Quintero <johandquintero@utp.edu.co>
2024-06-07 22:24:21 -0500

    add CORS, models and schemas files

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.comments.comments import router as comments_router
 from app.routers.posts.posts import router as posts_router
 from app.routers.users.users import router as users_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http:localhost:5173'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(comments_router)
 app.include_router(posts_router)
--- app/routers/comments/comments.py
+++ app/routers/comments/comments.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/comments",
                    )
 
-   
+
 @router.get("")
 def get_comments():
     return {"comment": "this is a comment", "commentId": "6j542e"}

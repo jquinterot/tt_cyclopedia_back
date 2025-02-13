9f58d71f929d965bb118e3397d8849da79c81000
Johany Quintero <johandquintero@utp.edu.co>
2025-02-13 12:30:10 -0500

    update connection secure

--- app/config/postgres_config.py
+++ app/config/postgres_config.py
@@ -6,7 +6,13 @@ import os
 load_dotenv()
 postgres_db = os.getenv("SQL_DB")
 
-engine = create_engine(postgres_db, echo=True)
+engine = create_engine(
+    postgres_db,
+    echo=True,
+    pool_pre_ping=True,  # Check connections before use
+    pool_recycle=300      # Recycle connections every 5 minutes
+)
+
 Base = declarative_base()
 
 event.listen(
